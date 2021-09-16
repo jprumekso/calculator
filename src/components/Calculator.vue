017ab94e411aba0dd557dd791e64e63e9bea77ed
Jalu Pujo <jalurumekso@gmail.com>
2021-09-16 16:48:37 +0700

    initial working formula

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,37 +1,92 @@
 <template>
   <!-- Input -->
-  <div class="input-group">
-    <input class="form-control me-3" type="number" name="" id="" />
-    <input type="checkbox" name="" id="" />
-  </div>
-  <div class="input-group">
-    <input class="form-control me-3" type="number" name="" id="" />
-    <input type="checkbox" name="" id="" />
-  </div>
-  <div class="input-group">
-    <input class="form-control me-3" type="number" name="" id="" />
+  <div v-for="input in inputs" :key="input.id" class="input-group">
+    <input
+      v-model.number="input.value"
+      class="form-control me-3"
+      type="number"
+      name=""
+      id=""
+    />
     <input type="checkbox" name="" id="" />
   </div>
 
   <!-- Operator -->
   <div class="operator-group">
-    <button class="operator-btn">+</button>
-    <button class="operator-btn">-</button>
-    <button class="operator-btn">x</button>
-    <button class="operator-btn">/</button>
+    <button @click="setOperator('addition')" class="operator-btn">+</button>
+    <button @click="setOperator('substraction')" class="operator-btn">-</button>
+    <button @click="setOperator('multiplication')" class="operator-btn">
+      x
+    </button>
+    <button @click="setOperator('division')" class="operator-btn">/</button>
   </div>
 
   <hr />
 
   <div class="result-box">
     <p>Hasil</p>
-    <p>1</p>
+    <p>{{ calcResult }}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Calculator',
+
+  data() {
+    return {
+      inputs: [
+        {
+          id: 1,
+          value: 0,
+        },
+        {
+          id: 2,
+          value: 0,
+        },
+        {
+          id: 5,
+          value: 0,
+        },
+      ],
+
+      selectedOperator: '',
+    };
+  },
+
+  computed: {
+
+    variables() {
+      return this.inputs.map((input) => input.value);
+    },
+
+    addition() {
+      return this.variables.reduce((acc, input) => acc + input);
+    },
+
+    substraction() {
+      return this.variables.reduce((acc, input) => acc - input);
+    },
+
+    division() {
+      return this.variables.reduce((acc, input) => acc / input);
+    },
+
+    multiplication() {
+      return this.variables.reduce((acc, input) => acc * input);
+    },
+
+    calcResult() {
+      return this.selectedOperator ? this[this.selectedOperator] : 0;
+    },
+
+  },
+
+  methods: {
+    setOperator(operator) {
+      this.selectedOperator = operator;
+    },
+  },
 };
 </script>
 
