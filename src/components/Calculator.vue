398b401469fb0e6707430db7b760b27973c5a31f
Jalu Pujo <jalurumekso@gmail.com>
2021-09-16 15:55:40 +0700

    First ui iteration

---
+++ src/components/Calculator.vue
@@ -0,0 +1,69 @@
+<template>
+  <!-- Input -->
+  <div class="input-group">
+    <input class="form-control me-3" type="number" name="" id="" />
+    <input type="checkbox" name="" id="" />
+  </div>
+  <div class="input-group">
+    <input class="form-control me-3" type="number" name="" id="" />
+    <input type="checkbox" name="" id="" />
+  </div>
+  <div class="input-group">
+    <input class="form-control me-3" type="number" name="" id="" />
+    <input type="checkbox" name="" id="" />
+  </div>
+
+  <!-- Operator -->
+  <div class="operator-group">
+    <button class="operator-btn">+</button>
+    <button class="operator-btn">-</button>
+    <button class="operator-btn">x</button>
+    <button class="operator-btn">/</button>
+  </div>
+
+  <hr />
+
+  <div class="result-box">
+    <p>Hasil</p>
+    <p>1</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calculator',
+};
+</script>
+
+<style>
+  .input-group {
+    display: flex;
+    margin-bottom: 0.75rem;
+  }
+
+  .input-group .form-control {
+    width: 100%;
+    padding: 0.5rem;
+  }
+
+  .operator-group {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+  }
+
+  .operator-btn {
+    width: 3rem;
+    height: 3rem;
+  }
+
+  .result-box {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .me-3 {
+    margin-right: 1rem;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <calculator></calculator>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Calculator from '@/components/Calculator.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    Calculator,
   },
 };
 </script>
+
+<style>
+  .main {
+    max-width: 240px;
+  }
+</style>
